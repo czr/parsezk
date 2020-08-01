c3cbdc665b2b07236bcce8a9bc6e1fca433b56c6
Colin Z. Robertson <czr@rtnl.org.uk>
2020-08-01 15:38:04 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='parsezk',
+      version='0.1',
+      description="Parse czr's idiosyncratic Zettelkästen structure",
+      url='http://github.com/czr/parsezk',
+      author='Colin Z. Robertson',
+      author_email='czr@rtnl.org.uk',
+      license='MIT',
+      packages=['parsezk'],
+      scripts=['zkgv'],
+      zip_safe=False)
